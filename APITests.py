9c59ce03269f462afa6cdd9c3522593fdeb0d908
AhmadAQ97 <62055033+AhmadAQ97@users.noreply.github.com>
2020-07-08 12:23:42 +0300

    Documentation

--- APITests.py
+++ APITests.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "https://reqres.in/"
+BASE_URL = "https://reqres.in/"  # This is the base URL used for all requests
 HEADERS = {
     'accept': 'application/json',
     'Content-Type': 'application/json',
@@ -9,36 +9,57 @@ HEADERS = {
 
 def get_all_users():
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided url to send an API request
+          using python's requests library (GET method). It should return the json data that contains
+          the specific data you are hoping to find. This function only returns the data with
+          the given path you provide (in tuple format).
+
+          returns:
+            response_status_code: The response status of the request
+            number_of_users: total number of users in the database
     """
     relative_url = "api/users"
     url = BASE_URL + relative_url
-    response = requests.request("GET", url, headers=HEADERS)
-    number_of_users = response.json()['total']
+    response = requests.request("GET", url, headers=HEADERS)  # Stores the response
+    number_of_users = response.json()['total']  # returns total number of users in the database
     response_status_code = response.status_code
     return response_status_code, number_of_users
 
 
 def get_single_user(user_id):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user_id to send an API request
+          using python's requests library (GET method). It should return the json data
+          that contains the specific data you are hoping to find. This function only returns the data with
+          the given path you provide (in tuple format).
+
+          parameters:
+            user_id: The id of the user to use as an endpoint.
+
+          returns:
+            received_id: The received id from the response.
+            response_status_code: Response status code from the request.
     """
     relative_url = "api/users/" + str(user_id)
     url = BASE_URL + relative_url
     response = requests.request("GET", url, headers=HEADERS)
     received_id = (response.json()['data'])['id']
-    return response.status_code, received_id
+    response_status_code = response.status_code
+    return response_status_code, received_id
 
 
 def single_user_not_found(user_id):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user_id to send an API request
+          using python's requests library (GET method). It should return the json data
+          that contains the specific data you are hoping to find. This function only returns
+          the data with the given path you provide (in tuple format).
+
+          parameters:
+            user_id: The id of the user to use as an endpoint.
+
+          returns:
+            response_status_code: Response status code from the request.
     """
     relative_url = "api/users/" + str(user_id)
     url = BASE_URL + relative_url
@@ -47,15 +68,24 @@ def single_user_not_found(user_id):
     return response.status_code
 
 
-def create_user(Name, Job):
+def create_user(user_name, user_job):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user_name and user_job post a request
+          using python's requests library (POST request). It Creates a new user with the given
+          parameters.This function only returns the data in tuple format.
+
+          parameters:
+            user_name:  The new user name to create for a user
+            user_job:   The new job to create for a user
+
+          returns:
+            created_name: The received name from the response after posting the request.
+            created_job: The received job from the response after posting the request.
+            response_status_code: Response status code from the request.
     """
     relative_url = "api/users"
     url = BASE_URL + relative_url
-    data = {'name': Name, 'job': Job}
+    data = {'name': user_name, 'job': user_job}
     response = requests.request("POST", url, json=data, headers=HEADERS)
     created_name = response.json()['name']
     created_job = response.json()['job']
@@ -63,26 +93,43 @@ def create_user(Name, Job):
     return created_name, created_job, response_status
 
 
-def update_user(ID, Name, Job):
+def update_user(ID, user_name, user_job):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user ID to update the specific user
+          using python's requests library (PUT method). It updates the specific user with the given parameters.
+          This function returns the response data (name, job) from the request in tuple format.
+
+          parameters:
+            ID:  The specific user id to updates his data
+            user_name:  The new user name to create for a user
+            user_job:   The new job to create for a user
+
+          returns:
+            updated_name: The updated name from the response after posting the request.
+            updated_job: The updated job from the response after posting the request.
+            response_status_code: Response status code from the request.
     """
     relative_url = "api/users/"
     url = BASE_URL + relative_url + str(ID)
-    data = {'name': Name, 'job': Job}
+    data = {'name': user_name, 'job': user_job}
     response = requests.request("PUT", url, headers=HEADERS, json=data)
     updated_name = response.json()['name']
     updated_job = response.json()['job']
-    return updated_name, updated_job
+    response_status = response.status_code
+    return updated_name, updated_job, response_status
 
 
 def delete_user(Id):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user ID to delete a specific user
+          using python's requests library (DELETE method). This function returns the response
+          status data.
+
+          parameters:
+            ID:  The specific user id to delete.
+
+          returns:
+            response_status_code: Response status code from the request.
     """
     relative_url = "api/users/"
     url = BASE_URL + relative_url + str(Id)
@@ -90,50 +137,85 @@ def delete_user(Id):
     return response.status_code
 
 
-def successful_registration(Email, Passsword):
+def successful_registration(user_email, user_password):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user user_email and user_password to register
+          using python's requests library (POST method). It sends email, password details.This function
+          returns the response data (registration id,registration token, response status code) from the
+          request in tuple format.
+
+          parameters:
+            user_email: The user email for registration
+            user_password: The user password for registration
+
+          returns:
+            created_id: The response id created from registration request.
+            created_token: The response token created from registration request.
+            response_status_code: Response status code from registration request.
     """
     relative_url = "api/register"
     url = BASE_URL + relative_url
-    data = {'email': Email, 'password': Passsword}
+    data = {'email': user_email, 'password': user_password}
+    response = requests.request("POST", url, headers=HEADERS, json=data)
+    created_id = response.json()['id']
+    created_token = response.json()['token']
+    return created_id, created_token, response.status_code
+
+
+def unsuccessful_registration(user_email, user_password):
+    """
+          Documentation: This function uses the provided user user_email and user_password to register
+          using python's requests library (POST method). It sends email, password details.This function
+          returns the response data (response status code) from the request.
+
+          parameters:
+            user_email: The user email for registration
+            user_password: The user password for registration
+
+          returns:
+            response_status_code: Response status code from registration request.
+    """
+    relative_url = "api/register"
+    url = BASE_URL + relative_url
+    data = {'email': user_email, 'password': user_password}
     response = requests.request("POST", url, headers=HEADERS, json=data)
     return response.status_code
 
 
-def unsuccessful_registration(Email, Passsword):
+def successful_login(user_email, user_password):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
-    """
-    relative_url = "api/register"
-    url = BASE_URL + relative_url
-    data = {'email': Email, 'password': Passsword}
-    response = requests.request("POST", url, headers=HEADERS, json=data)
-    return response.status_code
+          Documentation: This function uses the provided user user_email and user_password to login
+          using python's requests library (POST method). It sends email, password details.This function
+          returns the response data (login token, response status code) from the request in tuple format.
 
+          parameters:
+            user_email: The user email for login
+            user_password: The user password for login
 
-def successful_login(Email, Passsword):
-    """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          returns:
+            response_token: The response token from login request.
+            response_status_code: Response status code from registration request.
     """
     relative_url = "api/login"
     url = BASE_URL + relative_url
-    data = {'email': Email, 'password': Passsword}
+    data = {'email': user_email, 'password': user_password}
     response = requests.request("POST", url, headers=HEADERS, json=data)
-    return response.status_code
+    response_token = response.json()['token']
+    return response_token, response.status_code
 
 
 def unsuccessful_login(Email, Passsword):
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the provided user user_email and user_password to login
+          using python's requests library (POST method). It sends email, password details.This function
+          returns the response data (response status code) from the reques.
+
+          parameters:
+            user_email: The user email for login
+            user_password: The user password for login
+
+          returns:
+            response_status_code: Response status code from registration request.
     """
     relative_url = "api/login"
     url = BASE_URL + relative_url
@@ -144,9 +226,14 @@ def unsuccessful_login(Email, Passsword):
 
 def delayed_response():
     """
-          Documentation: Takes a value such as a string to search against a regular expression.
-          To search for a specific regex value input into exp_value, set "escape_regex_chars" to False:
-          escape_regex_chars=False.
+          Documentation: This function uses the url to send an API request
+          using python's requests library (GET request). It should return the json data
+          that contains the specific data you are hoping to find. This
+          function only returns the data with the given path you provide (in tuple format).
+
+          returns:
+            response_status_code: The response status of the request
+            number_of_users: total number of users in the database
     """
     url = BASE_URL + "api/users?delay=3"
     response = requests.get(url, headers=HEADERS)
--- Keywords.robot
+++ Keywords.robot
@@ -10,22 +10,39 @@ ${STATUS_CODE_NOT_FOUND}  404
 ${TOTAL_NUMBER_OF_USERS}  12
 *** Keywords ***
 Get All Users Test
+    [Tags]    API
+    [Documentation]
+    ...    Calls the API to return the response status code and total number of users.
+    ...    Expected result is the API returns a code 200 success and 12 total users.
     ${response_code}  ${users_total}  get all users
     should be equal as integers  ${response_code}  ${STATUS_CODE_OK}
     should be equal as integers  ${users_total}  ${TOTAL_NUMBER_OF_USERS}
 
 Get A Specific User Test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the response status code and reponse user id.
+    ...    Expected result is the API returns 200 status code success and the user id from the response.
     ${id}=  Evaluate  random.randint(1, 12)  modules=random
     ${recived_response}  ${sent_id}   get single user  ${id}
     should be equal as integers  ${recived_response}  ${STATUS_CODE_OK}
     should be equal as integers  ${id}  ${sent_id}
 
 single user not found Test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the response status code.
+    ...    Expected result is the API returns 404 status code not found.
     ${id}=  Evaluate  random.randint(13, 100)  modules=random
     ${recived_response}  single user not found  ${id}
     should be equal as integers  ${recived_response}  ${STATUS_CODE_NOT_FOUND}
 
 Create a New User and Verify it was created
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the response status code CREATED name amd job from the response body.
+    ...    Expected result is the API returns 201 status code created and name, job of the sent request
+    ...    is the same from the response data.
     ${created_name}  set variable  ahmad
     ${created_job}  set variable  Actor
     ${new_user_name}  ${new_user_job}  ${response_status}  create user  ${created_name}  ${created_job}
@@ -34,44 +51,89 @@ Create a New User and Verify it was created
     should be equal as strings  ${created_job}   ${new_user_job}
 
 Update User and Verify it has been updated
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the response status code, created name amd job from the response body.
+    ...    Expected result is the API returns 201 status code created and name, job of the sent request
+    ...    is the same from the response data.
     ${user_ID}  Evaluate  random.randint(1, 12)  modules=random
     ${new_name}  set variable  ahmad
     ${new_job}  set variable  Actor
-    ${updated_user_name}  ${updated_user_job}   update user  ${user_ID}  ${new_name}  ${new_job}
+    ${updated_user_name}  ${updated_user_job}   ${received_status}   update user  ${user_ID}  ${new_name}  ${new_job}
+    should be equal as integers  ${received_status}  ${STATUS_CODE_OK}
     should be equal as strings  ${new_name}   ${updated_user_name}
     should be equal as strings  ${new_job}   ${updated_user_job}
 
 
 Delete User and Verify it Has been Deleted
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
      ${User_ID}=  Evaluate  random.randint(1, 12)  modules=random
      ${received_response}  delete user  ${User_ID}
      should be equal as integers  ${received_response}  ${STATUS_CODE_NO_CONTENT}
 
 Successfull Registration Test
-    ${user_email}=  set variable  eve.holt@reqres.in
-    ${password}=  set variable  qwerty
-    ${verify_successful_registration}  successful registration  ${user_email}  ${password}
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
+    ${user_email}  set variable  eve.holt@reqres.in
+    ${password}  set variable  qwerty
+    ${created_id}   ${created_token}   ${verify_successful_registration}  successful registration  ${user_email}  ${password}
+    variable should exist  ${created_id}
+    should not be empty  ${created_token}
     should be equal as integers  ${verify_successful_registration}  ${STATUS_CODE_OK}
 
 UnSuccessfull Registration Test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
     ${user_email}  set variable  ahmad@reqers.in
     ${password}  set variable  qwerty
     ${verify_unSuccessful_registration}  unsuccessful registration  ${user_email}  ${password}
     should be equal as integers  ${verify_unSuccessful_registration}  ${STATUS_CODE_BAD_REQUEST}
 
 Successfull Login Test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
     ${user_email}  set variable  eve.holt@reqres.in
     ${password}  set variable  qwerty
-    ${verify_Successful_Login}  successful login  ${user_Email}  ${password}
+    ${response_token}   ${verify_Successful_Login}  successful login  ${user_Email}  ${password}
+    should not be empty  ${response_token}
     should be equal as integers  ${verify_successful_login}  ${STATUS_CODE_OK}
 
 UnSuccessfull login Test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
     ${user_Email}  set variable  ahmad@reqers.in
     ${password}  set variable  qwerty
     ${verify_unSuccessful_Login}  unsuccessful login  ${user_Email}  ${password}
     should be equal as integers  ${verify_unSuccessful_login}  ${STATUS_CODE_BAD_REQUEST}
 
 delayed response test
+    [Tags]    lpn
+    [Documentation]
+    ...    Calls the API to return the siblings of the LPN passed in. Expected result
+    ...    is the API returns the six LPNs before and the six LPNs after the one that was passed up.
+    ...    It returns the case tied to each LPN, the po tied to the case, and the current location
+    ...    of the LPN. NOTE - These wouldnt necessarily be populated.
     ${recived_response}  ${users_total}  delayed response
     should be equal as integers  ${recived_response}  ${STATUS_CODE_OK}
     should be equal as integers  ${users_total}  ${TOTAL_NUMBER_OF_USERS}
